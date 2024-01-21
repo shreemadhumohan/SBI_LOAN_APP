5f30b66c5fcaa254b24b553ae4e4f4217a7eb566
Madhu It <preetiyadav2131@gmail.com>
2024-01-21 12:30:35 +0530

    first commit

---
+++ src/test/java/in/madhumohanPro/AppTest.java
@@ -0,0 +1,20 @@
+package in.madhumohanPro;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
